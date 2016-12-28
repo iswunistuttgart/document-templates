75c6de603ba837643973a784e198ffb73f362c00
Philipp Tempel <mail@philipptempel.me>
2016-12-28 15:45:46 +0100

    Update build.py to also support building on Windows

--- build.py
+++ build.py
@@ -6,8 +6,10 @@ import os
 import re
 import shutil
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir_path)
+path_base = os.path.dirname(os.path.realpath(__file__))
+dir_src = os.path.join(path_base, 'src')
+dir_dist = os.path.join(path_base, 'dist')
+files = ['doctorate', 'student']
 
 p = re.compile('\\\\(input|include)\\{(?P<incfile>.*)\\}')
 
@@ -20,17 +22,17 @@ def process_file(the_file):
     return lines
 
 def process_line(the_line):
-    global p, dir_path
+    global p, path_base
 
     m = p.search(the_line)
 
     if m :
         inc_file = m.group('incfile')
 
-        inc_path = os.path.join(dir_path, 'src', inc_file + '.tex')
+        path_inc_file = os.path.join(path_base, 'src', inc_file + '.tex')
 
-        if os.path.isfile(inc_path):
-            f = open(inc_path, 'r')
+        if os.path.isfile(path_inc_file):
+            f = open(path_inc_file, 'r')
 
             try:
                 return ''.join(process_file(f)) + '\n'
@@ -39,11 +41,11 @@ def process_line(the_line):
 
     return the_line
 
-for file in glob.glob("src/*.cls"):
-    src_class = open(file, 'r')
-    tar_class = open(file.replace('src/', 'dist/'), 'w')
-    src_template = file.replace('.cls', '.tex')
-    tar_template = src_template.replace('src/', 'dist/')
+for file in files:
+    src_class = open(os.path.join(dir_src, file + '.cls'), 'r')
+    tar_class = open(os.path.join(dir_dist, file + '.cls'), 'w')
+    src_template = os.path.join(dir_src, file + '.tex')
+    tar_template = os.path.join(dir_dist, file + '.tex')
 
     try :
         tar_class.write(''.join(process_file(src_class)) + '\n')
