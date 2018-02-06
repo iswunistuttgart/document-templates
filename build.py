0e10720d69f22af96ca82215b3450f218f894a18
Philipp Tempel <mail@philipptempel.me>
2018-02-06 20:28:11 +0100

    Update build.py to only compile one file at a time
    Use of this will become more clear with the next commit as this pushes a Makefile

--- build.py
+++ build.py
@@ -1,61 +1,115 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # coding=utf-8
 
-import os
+"""
+All imports
+"""
+# Argument parsing
+import argparse
+# Proper regular expression building and matching
 import re
-import shutil
+# OS specific things like file handling
+import os
+# Handle the current build time in the final document
 import datetime
-import time
-
-path_base = os.path.dirname(os.path.realpath(__file__))
-dir_src = os.path.join(path_base, 'src')
-dir_dist = os.path.join(path_base, 'dist')
-files = ['iswstud.cls', 'iswartcl.cls', 'iswbook.cls', 'iswdctrt.cls', 'iswstud.tex', 'iswartcl.tex', 'iswbook.tex', 'iswdctrt.tex', 'iswstud.latexmkrc', 'iswartcl.latexmkrc', 'iswbook.latexmkrc', 'iswdctrt.latexmkrc', 'iswbib.bbx', 'bibliography.bib']
+# Nicer way of handling paths in python
+import pathlib
 
 
-p_incfile = re.compile('\\\\(input|include)\\{(?P<incfile>.*)\\}')
-p_time = re.compile('\\\\filedate\\{(?P<filedate>.*)\\}')
+def build():
+    """
+    This is the main function that builds the given file
+    :return:
+    """
+    parser = argparse.ArgumentParser(description='Build the given TeX class.')
+
+    parser.add_argument('cls', metavar='c', nargs='+', type=str,
+                        help='LaTeX class to compile')
+
+    parser.add_argument('-d', '--dest', dest='destination', type=str,
+                        help='Set build destination folder.', default='dist', action='store')
+
+    # Parse the arguments given
+    args = parser.parse_args()
+
+    # Loop over each given cls file
+    [process_file(f, dest=args.destination) for f in args.cls]
 
 
-def process_file(the_file):
-    lines = []
+def process_file(f, dest='dist'):
+    """
+    Process the given file
+    :param str c: Filename of the file to process
+    :param str dest: Destination directory name or path
+    :return void:
+    """
+    # Open the source file
+    ps = pathlib.Path(f)
+    # Open the target file
+    pd = pathlib.Path(dest + os.sep + ps.name)
 
-    for line in the_file:
-        lines.append(process_line(line))
+    # Open the file
+    try:
+        with ps.open() as s:
+            # Process the file
+            cnt = process_lines(s)
 
-    return lines
+            # Write the content to the new target file
+            pd.write_text(cnt)
+    except BaseException as err:
+        print('Error reading file \'{}\''.format(f))
 
-def process_line(the_line):
-    global p, path_base
 
-    m_incfile = p_incfile.search(the_line)
-    m_time = p_time.search(the_line)
+def process_lines(f):
+    """
+    Process lines of the given file
+    :param file f: A file object to read through
+    :return str: The processed file content
+    """
+    lns = []
 
-    if m_incfile :
-        inc_file = m_incfile.group('incfile')
+    # Loop over each line of the file
+    for l in f:
+        lns.append(process_line(l))
 
-        path_inc_file = os.path.join(path_base, 'src', inc_file + '.tex')
+    # Return the list of files
+    return ''.join(lns)
 
-        if os.path.isfile(path_inc_file):
-            f = open(path_inc_file, 'r')
 
-            try:
-                return ''.join(process_file(f))
-            finally:
-                f.close()
-    elif m_time:
-        return the_line.replace(m_time.group('filedate'), datetime.datetime.utcnow().strftime('%Y/%m/%d'))
+def process_line(l, replace_inc=None, replace_time=None):
+    """
+    Process a single line of text
+    :param str l: The line to process and look for \input, \include, or \filedate
+    :param re replace_inc: Regular expression to use to match the line of \include or \input
+    :param re replace_time: Regular expression to use to match the line of \filedate
+    :return str: The processed or original line
+    """
+    # Build the replacers if not given
+    if replace_inc is None:
+        replace_inc = re.compile('\\\\(input|include)\\{(?P<incfile>.*)\\}')
+    if replace_time is None:
+        replace_time = re.compile('\\\\filedate\\{(?P<filedate>.*)\\}')
 
-    return the_line
+    # Search for an include directive in the current line
+    inc_found = replace_inc.search(l)
+    # Search for a filedate directive in the current line
+    time_found = replace_time.search(l)
 
-for file in files:
-    src_file = open(os.path.join(dir_src, file), 'r')
-    dest_file = open(os.path.join(dir_dist, file), 'w')
+    # Found an include directive to replace?
+    if inc_found:
+        # Get the file that should be injected
+        inc_file = pathlib.Path('src' + os.sep + inc_found.group('incfile') + '.tex')
 
-    try :
-        dest_file.write(''.join(process_file(src_file)))
-    except Exception as e:
-        print(e)
-    finally:
-        src_file.close()
-        dest_file.close()
+        # If the file exists, then process it
+        if inc_file.exists():
+            return ''.join(process_lines(inc_file.open()))
+    # Found a filedate directive to replace
+    elif time_found:
+        return l.replace(time_found.group('filedate'), datetime.datetime.utcnow().strftime('%Y/%m/%d'))
+
+    # Return the current line
+    return l
+
+
+if __name__ == '__main__':
+    build()
