fd3e3e7534bf594bcbc4633c1b84c9bcc0044b50
Philipp Tempel <mail@philipptempel.me>
2018-04-22 13:13:55 +0200

    Update build script to be more cleanly syntaxed

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # coding=utf-8
+# -*- coding: utf-8 -*-
 
 """
 All imports
@@ -50,9 +51,12 @@ def process_file(f, dest='build'):
 
     # Open the file
     try:
-        with ps.open() as s:
-            # Process the file
-            cnt = process_lines(s)
+        with ps.open('r') as s:
+            # Read the whole file
+            lns = s.readlines()
+
+            # Then process all lines
+            cnt = process_lines(lns)
 
             # Check if the target directory of the current file exists
             if not pd.parent.exists():
@@ -60,25 +64,20 @@ def process_file(f, dest='build'):
                 os.makedirs(pd.parent.absolute())
 
             # Write the content to the new target file
-            pd.write_text(cnt)
+            pd.write_text(''.join(cnt))
     except BaseException as err:
         raise ReferenceError('Error processing file \'{}\''.format(f)) from err
 
 
-def process_lines(f):
+def process_lines(lns, replace_inc=None, replace_time=None):
     """
     Process lines of the given file
-    :param file f: A file object to read through
+    :param list lns: An array of file lines
     :return str: The processed file content
     """
-    lns = []
 
-    # Loop over each line of the file
-    for l in f:
-        lns.append(process_line(l))
-
-    # Return the list of files
-    return ''.join(lns)
+    # Loop over each line of the file and return the new list
+    return [process_line(l, replace_inc, replace_time) for l in lns]
 
 
 def process_line(l, replace_inc=None, replace_time=None):
@@ -107,7 +106,7 @@ def process_line(l, replace_inc=None, replace_time=None):
 
         # If the file exists, then process it
         if inc_file.exists():
-            return ''.join(process_lines(inc_file.open()))
+            return ''.join(process_lines(inc_file.open('r').readlines(), replace_inc, replace_time))
     # Found a filedate directive to replace
     elif time_found:
         return l.replace(time_found.group('filedate'), datetime.datetime.utcnow().strftime('%Y/%m/%d'))
